187af57f4ef93c5438e5503ec21c7d93821800ff
kilowen <kilowen@126.com>
2016-02-15 16:28:12 +0800

    第二章 一切都是对象

---
+++ tij-EveryObjects/src/main/java/com/chang/eo/test/MyClass.java
@@ -0,0 +1,34 @@
+package com.chang.eo.test;
+
+public class MyClass {
+
+	private int a;
+	private String b;
+	private boolean c;
+	private char d;
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public String getB() {
+		return b;
+	}
+	public void setB(String b) {
+		this.b = b;
+	}
+	public boolean getC() {
+		return c;
+	}
+	public void setC(boolean c) {
+		this.c = c;
+	}
+	public char getD() {
+		return d;
+	}
+	public void setD(char d) {
+		this.d = d;
+	}
+}
---
+++ tij-EveryObjects/src/main/java/com/chang/eo/test/MyTest.java
@@ -0,0 +1,21 @@
+package com.chang.eo.test;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+
+		/*System.err.println(Integer.MAX_VALUE);
+		System.err.println(Integer.MIN_VALUE);*/
+
+		/*MyClass mc = new MyClass();
+		System.out.println(mc.getA());
+		System.out.println(mc.getB());
+		System.out.println(mc.getC());
+		System.out.println(mc.getD());
+		System.out.println('\u0000');*/
+
+		char c = '\u0000';
+		System.out.println("-" + c + "-");
+
+	}
+}
